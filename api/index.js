a6f9d38d5811adc08c5ea2436d2f787205e858e8
Mr Moısh <v.y.moish@gmail.com>
2024-11-18 10:48:27 -0300

    chore: подключение к mongodb

--- api/index.js
+++ api/index.js
@@ -1,3 +1,27 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: process.env.MONGODB_URI });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1
+  }
+});
+
+
+ export  default async function handler(req, res) {
+  // try {
+  //   // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const collection = client.db('box').collection('exampleCollection');
+
+    const results = await collection.find().toArray();
+
+  //   console.log(results);
+  // } finally {
+  //   // Ensures that the client will close when you finish/error
+  //   await client.close();
+  // }
+    res.status(200).json(results);
 }
